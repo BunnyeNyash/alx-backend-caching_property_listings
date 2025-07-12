1517adcf31f35eb20ffa73e089a0eceeed538755
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-13 01:54:11 +0300

    Update views.py to use the get_all_properties

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from .models import Property
+# from .models import Property
+from .utils import get_all_properties
 
 # Create your views here.
+# @cache_page(60 * 15)  # Cache for 15 minutes
+# def property_list(request):
+#     properties = Property.objects.all()
+#     return render(request, 'properties/property_list.html', {'properties': properties})
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all()
+    properties = get_all_properties()
     return render(request, 'properties/property_list.html', {'properties': properties})
